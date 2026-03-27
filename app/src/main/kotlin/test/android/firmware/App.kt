aec2530595e4de1997867815131f328cf1564ae7
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-27 12:59:00 +0300

    init android

---
+++ app/src/main/kotlin/test/android/firmware/App.kt
@@ -0,0 +1,9 @@
+package test.android.firmware
+
+import android.app.Application
+
+internal class App : Application() {
+    override fun onCreate() {
+        // todo
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "FirmwareSample"
+
+include("app")
